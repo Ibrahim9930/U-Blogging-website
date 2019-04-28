4000dd3616267ac9a84e71f2a9c58863122bd0f3
ibrahim <msri.ibraheem@gmail.com>
2019-04-28 18:34:55 +0300

    Google Accounts
    Yays and Nays fixed

--- web_project/categories/urls.py
+++ web_project/categories/urls.py
@@ -7,4 +7,5 @@ urlpatterns=[
     path('<name>',views.Categoryblogs.as_view(),name="category_blogs"),
     path('subscribe/<name>',views.Subscribe,name="subscribe"),
     path('unsubscribe/<name>',views.Unsubscribe,name="unsubscribe"),
+    path("cat_list/",views.CategoryList.as_view(),name="list"),
 ]

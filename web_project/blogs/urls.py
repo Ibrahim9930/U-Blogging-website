0f9e2a73df21856b8323957d1101dfa1355d7dad
ibrahim <msri.ibraheem@gmail.com>
2019-04-28 14:34:32 +0300

    added explore view

--- web_project/blogs/urls.py
+++ web_project/blogs/urls.py
@@ -5,11 +5,12 @@ app_name='blogs'
 
 urlpatterns=[
     path('createblog/', views.CreateBlog.as_view(), name='create'),
-    path('<username>', views.Uerblogs.as_view(), name='uerblogs'),
     path('edit/<int:pk>', views.EditBlog.as_view(), name='edit'),
     path('<int:pk>/', views.BlogDetails.as_view(), name="details"),
     path('delete/<int:pk>', views.DeleteBlog.as_view(), name="delete"),
     path('home/', views.HomePage.as_view(), name="homepage"),
+    path('<username>', views.Uerblogs.as_view(), name='uerblogs'),
+    path("explore/", views.ExploreBlogs.as_view(), name="explore"),
     path('yay/', views.yay, name="yay"),
     path('unyay/', views.unyay, name="unyay"),
     path('nay/', views.nay, name="nay"),

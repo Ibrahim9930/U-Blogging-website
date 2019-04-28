43e4dc7ecc52551fe0351dba504b5b2acce241f8
ramideeb <35407681+ramideeb@users.noreply.github.com>
2019-04-28 16:24:53 +0300

    control panel is done

---
+++ web_project/php/controlpanel/Users/activate.php
@@ -0,0 +1,17 @@
+<?php
+class MyDB extends SQLite3 {
+   function __construct() {
+     $this->open('..\..\..\db.sqlite3');
+   }
+}
+
+if(isset($_POST['id']))
+{
+    $db = new MyDB();
+    $query = $db->exec( ' UPDATE auth_user SET is_active=1 WHERE username="'.$_POST["id"].'"' );
+
+    header("Refresh:0; url=index.php");
+
+}
+
+ ?>

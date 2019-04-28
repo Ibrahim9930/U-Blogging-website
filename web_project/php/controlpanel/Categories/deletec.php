71dcc68fe3a603aa70a3ee3bce3b62a1044671a3
ramideeb <35407681+ramideeb@users.noreply.github.com>
2019-04-28 15:54:59 +0300

    control panel modified

--- web_project/php/controlpanel/Categories/deletec.php
+++ web_project/php/controlpanel/Categories/deletec.php
@@ -8,6 +8,19 @@ class MyDB extends SQLite3 {
 if(isset($_POST['userID']))
 {
     $db = new MyDB();
+
+    $query =  "SELECT * FROM blogs_blog where category_id=".$_POST['userID'];     // to change whith post id
+    $ret = $db->query($query);
+      while($row = $ret->fetchArray(SQLITE3_ASSOC) )
+       {
+         $db->exec('DELETE FROM blogs_nay WHERE nayed_id='.$row['id']);
+         $db->exec('DELETE FROM blogs_yay WHERE yayed_id='.$row['id']);
+         $db->exec('DELETE FROM blogs_blog  WHERE id='.$row['id']);
+        }
+
+
+        $db->exec('DELETE FROM categories_subscriber  WHERE category_id='.$_POST['userID']);
+
     $db->exec( 'DELETE FROM categories_category WHERE id='.$_POST['userID']);
     header("Refresh:0; url=home.php");
 
--- web_project/php/controlpanel/Posts/postdelete.php
+++ web_project/php/controlpanel/Posts/postdelete.php
@@ -8,7 +8,10 @@ class MyDB extends SQLite3 {
 if(isset($_POST['id']))
 {
     $db = new MyDB();
+    $db->exec('DELETE FROM blogs_nay WHERE nayed_id='.$_POST['id']);
+    $db->exec('DELETE FROM blogs_yay WHERE yayed_id='.$_POST['id']);
     $db->exec('DELETE FROM blogs_blog WHERE id='.$_POST['id']);
+    
     header("Refresh:0; url=index.php");
 
 }
